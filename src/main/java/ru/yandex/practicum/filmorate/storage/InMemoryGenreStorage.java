51536bf954fb5bd433b489f1c1e52105a5db9b4a
Skzo <francesco@friargiu.ru>
2025-07-24 00:31:17 +0300

    спринт 12 ver. 0.17

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryGenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryGenreStorage.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.Genre;
+
 import java.util.List;
 
 public class InMemoryGenreStorage implements GenreStorage {
@@ -8,6 +9,7 @@ public class InMemoryGenreStorage implements GenreStorage {
     public Genre findById(int id) {
         return new Genre(id, "Test-Genre");
     }
+
     @Override
     public List<Genre> findAll() {
         return List.of(new Genre(1, "Test-Genre"));
--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryMpaStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryMpaStorage.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.Mpa;
+
 import java.util.List;
 
 public class InMemoryMpaStorage implements MpaStorage {
@@ -8,6 +9,7 @@ public class InMemoryMpaStorage implements MpaStorage {
     public Mpa findById(int id) {
         return new Mpa(id, "Test-MPA");
     }
+
     @Override
     public List<Mpa> findAll() {
         return List.of(new Mpa(1, "Test-MPA"));
